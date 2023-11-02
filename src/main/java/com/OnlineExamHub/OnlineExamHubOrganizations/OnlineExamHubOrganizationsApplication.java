a9dce2194f2bbf1d53841432e6653b964245cd77
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2023-11-03 00:37:12 +0530

    initial commit.

---
+++ src/main/java/com/OnlineExamHub/OnlineExamHubOrganizations/OnlineExamHubOrganizationsApplication.java
@@ -0,0 +1,13 @@
+package com.OnlineExamHub.OnlineExamHubOrganizations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamHubOrganizationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamHubOrganizationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OnlineExamHub/OnlineExamHubOrganizations/OnlineExamHubOrganizationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineExamHub.OnlineExamHubOrganizations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamHubOrganizationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
